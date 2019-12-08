111a881e5b9fc2e6b17f434b0ea7b2e847759428
liyongpeng007 <15201541585@163.com>
2019-12-08 22:38:27 +0800

    提交Chapter9文件和pom.xml文件

---
+++ Chapter9/src/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,24 @@
+package com.course.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+    @Test
+    public void test1() throws IOException {
+        //设置一个存储值的字符串
+        String result;
+        //要访问的路径
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
