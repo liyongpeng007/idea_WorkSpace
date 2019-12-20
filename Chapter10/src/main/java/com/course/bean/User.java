edf9bf2d723b13e5967e32e82a8b70b134cf4c45
liyongpeng007 <15201541585@163.com>
2019-12-20 11:13:27 +0800

    提交Chapter10

--- Chapter10/src/main/java/com/course/bean/User.java
+++ Chapter10/src/main/java/com/course/bean/User.java
@@ -1,4 +1,71 @@
 package com.course.bean;
 
-public class User {
+import lombok.Data;
+
+@Data
+public class User  {
+    private String userName;
+    private String passWord;
+    private String name;
+    private String age;
+    private String sex;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public User(String userName, String passWord, String name, String age, String sex) {
+        this.userName = userName;
+        this.passWord = passWord;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
 }
--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,4 +1,50 @@
 package com.course.server;
 
+import com.course.bean.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@Api(value = "/",description = "这是我全部的post请求")
+@RequestMapping("/v1")
 public class MyPostMethod {
+    //这个变量用来装我们cookies信息的
+    private static Cookie cookie;
+
+    //用户登录成功获取到cookies,然后再访问其他接口获取到列表
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ApiOperation(value = "登录成功，成功后获取cookies信息",httpMethod = "POST")
+    public String login(HttpServletResponse response,
+                        @RequestParam(value = "userName",required = true) String userName,
+                        @RequestParam(value = "passWord",required = true) String passWord
+                        ){
+        if(userName.equals("zhangsan") && passWord.equals("123456")){
+            cookie = new Cookie("login","true");
+            response.addCookie(cookie);
+            return "恭喜你获得cookie成功！";
+        }
+        return "用户名或者密码错误！！！";
+    }
+
+    @RequestMapping(value = "/getUserList",method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表",httpMethod = "POST")
+    public String getUserList(HttpServletRequest request, @RequestBody User user){
+        //获取cookies
+        Cookie[] cookies = request.getCookies();
+        //验证cookies是否合法
+        for (Cookie c : cookies){
+            if (c.getName()=="login" && c.getValue()=="true" && user.getUserName()=="zhangsan" && user.getPassWord()=="123456"){
+                user.setName("lisi");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+            }
+        }
+        return "参数不合法";
+    }
 }
