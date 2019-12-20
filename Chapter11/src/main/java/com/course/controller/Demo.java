5ab69db6d0c0f62f397dfbd4ff509fd7a91e8e57
liyongpeng007 <15201541585@163.com>
2019-12-20 11:12:16 +0800

    提交Chapter11

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,17 +1,15 @@
-package com.course.model;
+package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-//import lombok.extern.log4j.Log4j;
+
+import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-
-//@Log4j
+@Log4j2
 @RestController
 @Api(value = "v1",description = "这是我的第一个版本的demo")
 @RequestMapping("v1")
@@ -21,10 +19,37 @@ public class Demo {
     @Autowired
     private SqlSessionTemplate template;
 
+    /**
+     * 查询一共几条数据
+     * */
     @RequestMapping(value = "/getUserCount",method = RequestMethod.GET)
     @ApiOperation(value = "可以获取到用户数",httpMethod = "GET")
     public int getUserCount(){
-        //return template.selectOne("getUserCount");
         return template.selectOne("getUserCount");
+    }
+
+    /**
+     * 添加方法
+     * */
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public int addUser(@RequestBody User user){
+        int result = template.insert("addUser",user);
+        return result;
+    }
+
+    /**
+     * 修改方法
+     * */
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+
+    /**
+     * 删除方法
+     * */
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET)
+    public int delUser(@RequestParam int id){
+        return template.delete("deleteUser",id);
     }
 }
